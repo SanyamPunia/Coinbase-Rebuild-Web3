2e1d00251120f08d3f964d306b5db310fb6ea3ec
SanyamPunia <lewarends@gmail.com>
2022-01-29 04:01:40 +0530

    add metamask authentication using thirdweb, header component

---
+++ pages/Dashboard.js
@@ -0,0 +1,29 @@
+import Main from '../components/Main';
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+const Dashboard = ({ address }) => {
+  return (
+    <Wrapper>
+      <MainContainer>
+        <Header />
+        <Main />
+      </MainContainer>
+    </Wrapper>
+  )
+};
+
+export default Dashboard;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
